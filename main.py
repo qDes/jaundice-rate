a371d20f24e59970f57b1d3af0e07e333cbbaa35
Andrei K <1____1@bk.ru>
2020-01-24 12:42:41 +0300

    process ClientError

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pymorphy2
 
 from adapters.inosmi_ru import sanitize
 from aionursery import Nursery
+from aiohttp.client_exceptions import ClientConnectionError
 from enum import Enum
 from text_tools import calculate_jaundice_rate, split_by_words
 from text_tools import load_charged_words
@@ -13,6 +14,9 @@ class ProcessingStatus(Enum):
     OK = 'OK'
     FETCH_ERROR = 'FETCH_ERROR'
 
+    def __str__(self):
+        return str(self.value)
+
 
 async def fetch(session, url):
     async with session.get(url) as response:
@@ -21,12 +25,26 @@ async def fetch(session, url):
 
 
 async def process_article(session, morph, charged_words, url, title):
-    html = await fetch(session, url)
-    sanitized_html = sanitize(html, plaintext=True)
-    article_words = split_by_words(morph, sanitized_html)
-    score = calculate_jaundice_rate(article_words, charged_words)
-    words_count = len(article_words)
-    return (title, score, words_count)
+    try:
+        html = await fetch(session, url)
+        sanitized_html = sanitize(html, plaintext=True)
+        article_words = split_by_words(morph, sanitized_html)
+        score = calculate_jaundice_rate(article_words, charged_words)
+        words_count = len(article_words)
+        status = ProcessingStatus.OK
+    except ClientConnectionError:
+        return (title, ProcessingStatus.FETCH_ERROR, None, None)
+    return (title,status, score, words_count)
+
+
+def process_results(results):
+    for result in results:
+        outcome = result.result()
+        print("Article name", outcome[0])
+        print("Processing status", outcome[1])
+        print("Article score", outcome[2])
+        print("Words count", outcome[3])
+
 
 
 async def main():
@@ -48,6 +66,8 @@ async def main():
             results = await asyncio.wait(tasks)
         for result in results[0]:
             print(result.result())
+
+        process_results(results[0])
 
 
 if __name__ == "__main__":
