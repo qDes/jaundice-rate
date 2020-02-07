9427bcd02bc8c981aa36194f76049484ee3a2d15
Andrei K <1____1@bk.ru>
2020-02-07 11:41:41 +0300

    add time to word analyze

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import aiohttp
 import asyncio
+import logging
 import pymorphy2
+import time
 
 from adapters.inosmi_ru import sanitize
 from adapters.exceptions import ArticleNotFound
 from aionursery import Nursery
 from aiohttp.client_exceptions import ClientConnectionError
 from async_timeout import timeout
+from contextlib import contextmanager
 from enum import Enum
 from text_tools import calculate_jaundice_rate, split_by_words
 from text_tools import load_charged_words
@@ -22,6 +25,12 @@ class ProcessingStatus(Enum):
         return str(self.value)
 
 
+async def analyzator(morph, html):
+    words = await split_by_words(morph, html)
+    analyze_time = time.monotonic()
+    return words, analyze_time
+
+
 async def fetch(session, url):
     async with session.get(url) as response:
         response.raise_for_status()
@@ -29,21 +38,25 @@ async def fetch(session, url):
 
 
 async def process_article(session, morph, charged_words, url, title):
+    score = None
+    words_count = None
+    time_analyze = None
     try:
         async with timeout(1.5) as tm:
             html = await fetch(session, url)
         sanitized_html = sanitize(html, plaintext=True)
-        article_words = split_by_words(morph, sanitized_html)
+        async with timeout(1) as tm:
+            article_words, time_analyze = await analyzator(morph, sanitized_html)
         score = calculate_jaundice_rate(article_words, charged_words)
         words_count = len(article_words)
         status = ProcessingStatus.OK
     except ClientConnectionError:
-        return (title, ProcessingStatus.FETCH_ERROR, None, None)
+        status = ProcessingStatus.FETCH_ERROR
     except ArticleNotFound:
-        return (title, ProcessingStatus.PARSING_ERROR, None, None)
+        status = ProcessingStatus.PARSING_ERROR
     except asyncio.TimeoutError:
-        return (title, ProcessingStatus.TIMEOUT, None, None)
-    return (title,status, score, words_count)
+        status = ProcessingStatus.TIMEOUT
+    return (title,status, score, words_count, time_analyze)
 
 
 def process_results(results):
@@ -53,6 +66,8 @@ def process_results(results):
         print("Processing status", outcome[1])
         print("Article score", outcome[2])
         print("Words count", outcome[3])
+        logging.debug(f"Analyze time {str(outcome[4])}")
+        print()
 
 
 
@@ -64,6 +79,8 @@ async def main():
             'http://sasifsd45eegdfi.com',
             'https://lenta.ru/news/2020/01/24/voting/'
             ]
+    FORMAT = "%(levelname)s:root: %(message)s"
+    logging.basicConfig(format=FORMAT, level=logging.DEBUG)
     morph = pymorphy2.MorphAnalyzer()
     charged_words = load_charged_words('charged_dict/negative_words.txt')
     async with aiohttp.ClientSession() as session:
